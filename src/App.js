14a043f32d8d82f02e54fc96a7a3cfef2198b721
Alik64 <albert.barsamov@gmail.com>
2022-05-06 15:22:08 +0200

    instalation of react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const App = () => {
-  console.log("Hello from App.js!");
+  return <div>Hello React!!!</div>;
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { App } from "./App";
-import Header from "./components/Header/Header";
-Header();
-App();
-console.log("Hello mfrs! Let's go!");
+
+ReactDOM.render(<App />, document.getElementById("root"));
